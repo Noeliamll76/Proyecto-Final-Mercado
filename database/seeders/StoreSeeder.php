c5b8e39c875ed0eeab13baa5c14f2bb23a78701e
Noeliamll76 <135754377+Noeliamll@users.noreply.github.com>
2023-12-29 03:54:52 +0100

    Feat: Add dates

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -17,13 +17,12 @@ class StoreSeeder extends Seeder
         DB::table('stores')->insert(
             [
                 'name' => Str::random(10),
-                'address' => 'Avd. Blasco Ibañez, 122, pta 7',
-                'zip_code' => '46022',
-                'town' => 'Valencia',
-                'phone' => '666777888',
-                'DNI' => '66777888L',
-                'birthdate' => '2000/11/13',
-                'email' => 'admin@admin.com',
+                'owner'=> "Paco Martinez",
+                'location' => 'Puestos 1-4 Nave 3',
+                'guild_id' => '1',
+                'is_active' => true,
+                'image' => 'https://tse1.mm.bing.net/th?id=OIP.WDyTNPoKCAFAgbVT-4dSagHaEb&pid=Api&P=0&h=180',
+                'email' => 'paco@paco.com',
                 'password' => Hash::make('password'),
                 'roles' => "admin"
             ]
@@ -32,61 +31,17 @@ class StoreSeeder extends Seeder
         DB::table('users')->insert(
             [
                 'name' => Str::random(10),
-                'address' => 'C/ Marino Sirera, 12, pta 17',
-                'zip_code' => '46011',
-                'town' => 'Valencia',
-                'phone' => '600999888',
-                'DNI' => '60099988L',
-                'birthdate' => '1990/08/15',
-                'email' => 'superadmin@superadmin.com',
-                'password' => Hash::make('password'),
-                'roles' => "superAdmin"
-            ]
-        );
-
-        DB::table('users')->insert(
-            [
-                'name' => Str::random(10),
-                'address' => 'C/ Marino Sirera, 6, pta 21',
-                'zip_code' => '46011',
-                'town' => 'Valencia',
-                'phone' => '600999888',
-                'DNI' => '60099988L',
-                'birthdate' => '1990/02/26',
-                'email' => 'paco@paco.com',
-                'password' => Hash::make('password'),
-                'roles' => "user"
-            ]
-        );
-
-        DB::table('users')->insert(
-            [
-                'name' => 'Loli',
-                'address' => 'C/ Escalante, 12, bajo',
-                'zip_code' => '46011',
-                'town' => 'Valencia',
-                'phone' => '600999888',
-                'DNI' => '60099988L',
-                'birthdate' => '1964/04/04',
+                'owner'=> "Loli Panoli",
+                'location' => 'Puestos 24-30 Nave 1',
+                'guild_id' => '8',
+                'is_active' => true,
+                'image' => 'https://tse3.mm.bing.net/th?id=OIP.qNc322sGXXkDHVzmZBrjhgHaEv&pid=Api&P=0&h=180',
                 'email' => 'loli@loli.com',
                 'password' => Hash::make('password'),
-                'roles' => "user"
+                'roles' => "admin"
             ]
         );
-
-        DB::table('users')->insert(
-            [
-                'name' => 'Manuela',
-                'address' => 'C/ Rosario, 122, bajo',
-                'zip_code' => '46011',
-                'town' => 'Valencia',
-                'phone' => '600999888',
-                'DNI' => '60099988L',
-                'birthdate' => '1964/07/19',
-                'email' => 'manuela@manuela.com',
-                'password' => Hash::make('password'),
-                'roles' => "user"
-            ]
-        );
+       
+        
     }
 }
